ff794304dfda2ac9b9206f18199e888a958399ed
sevase <108822198+SeVaSe@users.noreply.github.com>
2023-10-19 10:46:56 +0300

    переход к меню настроено ОТЛИЧНО

--- game_circle_reaction.py
+++ game_circle_reaction.py
@@ -1,3 +1,4 @@
+import subprocess
 import mediapipe as mp
 import cv2
 import numpy as np
@@ -66,105 +67,113 @@ def run_circle_raction_game():
         red_circles.clear()
 
     video = cv2.VideoCapture(0)
+    try:
+        with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+            while video.isOpened():
+                _, frame = video.read()
 
-    with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
-        while video.isOpened():
-            _, frame = video.read()
+                if not game_over:
+                    elapsed_time = time.time() - start_time
+                    if elapsed_time > time_limit:
+                        game_over_screen()
 
-            if not game_over:
-                elapsed_time = time.time() - start_time
-                if elapsed_time > time_limit:
-                    game_over_screen()
+                image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                image = cv2.flip(image, 1)
+                imageHeight, imageWidth, _ = image.shape
 
-            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            image = cv2.flip(image, 1)
-            imageHeight, imageWidth, _ = image.shape
+                results = hands.process(image)
+                image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-            results = hands.process(image)
-            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+                font = cv2.FONT_HERSHEY_COMPLEX
+                color = (255, 0, 255)
+                text = cv2.putText(image, "Уровень: " + str(level), (240, 30), font, 1, color, 2, cv2.LINE_AA)
+                text = cv2.putText(image, "Очки: " + str(score), (460, 30), font, 1, color, 2, cv2.LINE_AA)
+                text = cv2.putText(image, "Жизни: " + str(lives), (30, 30), font, 1, color, 2, cv2.LINE_AA)
 
-            font = cv2.FONT_HERSHEY_COMPLEX
-            color = (255, 0, 255)
-            text = cv2.putText(image, "Уровень: " + str(level), (240, 30), font, 1, color, 2, cv2.LINE_AA)
-            text = cv2.putText(image, "Очки: " + str(score), (460, 30), font, 1, color, 2, cv2.LINE_AA)
-            text = cv2.putText(image, "Жизни: " + str(lives), (30, 30), font, 1, color, 2, cv2.LINE_AA)
+                if not enemies and not game_over:
+                    generate_enemies(num_enemies)
 
-            if not enemies and not game_over:
-                generate_enemies(num_enemies)
+                if not red_circles and not game_over and score >= 5:
+                    generate_red_circles(1)
 
-            if not red_circles and not game_over and score >= 5:
-                generate_red_circles(1)
+                enemy()
+                red_circle()
 
-            enemy()
-            red_circle()
+                if not game_over and results.multi_hand_landmarks:
+                    for num, hand in enumerate(results.multi_hand_landmarks):
+                        mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
+                                                  mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
 
-            if not game_over and results.multi_hand_landmarks:
-                for num, hand in enumerate(results.multi_hand_landmarks):
-                    mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS,
-                                              mp_drawing.DrawingSpec(color=(250, 44, 250), thickness=2, circle_radius=2))
+                        for point in mp_hands.HandLandmark:
+                            normalizedLandmark = hand.landmark[point]
+                            if normalizedLandmark is not None:
+                                pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x,
+                                                                                                   normalizedLandmark.y,
+                                                                                                   imageWidth, imageHeight)
 
-                    for point in mp_hands.HandLandmark:
-                        normalizedLandmark = hand.landmark[point]
-                        if normalizedLandmark is not None:
-                            pixelCoordinatesLandmark = mp_drawing._normalized_to_pixel_coordinates(normalizedLandmark.x,
-                                                                                               normalizedLandmark.y,
-                                                                                               imageWidth, imageHeight)
+                                if point == mp_hands.HandLandmark.INDEX_FINGER_TIP:
+                                    cv2.circle(image, (pixelCoordinatesLandmark[0], pixelCoordinatesLandmark[1]), 25, (250, 196, 0),
+                                               5)
+                                    for i, (x_enemy, y_enemy) in enumerate(enemies):
+                                        if abs(pixelCoordinatesLandmark[0] - x_enemy) < 25 and abs(
+                                                pixelCoordinatesLandmark[1] - y_enemy) < 25:
+                                            del enemies[i]
+                                            score += 1
+                                    for i, (x_red, y_red) in enumerate(red_circles):
+                                        if abs(pixelCoordinatesLandmark[0] - x_red) < 25 and abs(
+                                                pixelCoordinatesLandmark[1] - y_red) < 25:
+                                            del red_circles[i]
+                                            lives -= 1
 
-                            if point == mp_hands.HandLandmark.INDEX_FINGER_TIP:
-                                cv2.circle(image, (pixelCoordinatesLandmark[0], pixelCoordinatesLandmark[1]), 25, (250, 196, 0),
-                                           5)
-                                for i, (x_enemy, y_enemy) in enumerate(enemies):
-                                    if abs(pixelCoordinatesLandmark[0] - x_enemy) < 25 and abs(
-                                            pixelCoordinatesLandmark[1] - y_enemy) < 25:
-                                        del enemies[i]
-                                        score += 1
-                                for i, (x_red, y_red) in enumerate(red_circles):
-                                    if abs(pixelCoordinatesLandmark[0] - x_red) < 25 and abs(
-                                            pixelCoordinatesLandmark[1] - y_red) < 25:
-                                        del red_circles[i]
-                                        lives -= 1
+                # Проверка касаний бортов окна
+                if not game_over and pixelCoordinatesLandmark is not None:
+                    if pixelCoordinatesLandmark[0] < 50 or pixelCoordinatesLandmark[0] > 600 or \
+                       pixelCoordinatesLandmark[1] < 50 or pixelCoordinatesLandmark[1] > 600:
+                        lives -= 1
+                    if lives <= 0:
+                        game_over_screen()
+                        game_over = True
+                        cv2.destroyAllWindows()
+                        print(f"Ты набрал: {score} очка")
+                        sys.exit()  # Завершить программу
 
-            # Проверка касаний бортов окна
-            if not game_over and pixelCoordinatesLandmark is not None:
-                if pixelCoordinatesLandmark[0] < 50 or pixelCoordinatesLandmark[0] > 600 or \
-                   pixelCoordinatesLandmark[1] < 50 or pixelCoordinatesLandmark[1] > 600:
-                    lives -= 1
-                if lives <= 0:
-                    game_over_screen()
-                    game_over = True
-                    cv2.destroyAllWindows()
-                    print(f"Ты набрал: {score} очка")
-                    sys.exit()  # Завершить программу
+                # Двигаем красные кружки вверх
+                if not game_over:
+                    for i in range(len(red_circles)):
+                        red_circles[i] = (red_circles[i][0], red_circles[i][1] - 5)
 
-            # Двигаем красные кружки вверх
-            if not game_over:
-                for i in range(len(red_circles)):
-                    red_circles[i] = (red_circles[i][0], red_circles[i][1] - 5)
+                        # Если красный кружок вышел за границу экрана, удаляем его
+                        if red_circles[i][1] < 0:
+                            del red_circles[i]
+                            break
 
-                    # Если красный кружок вышел за границу экрана, удаляем его
-                    if red_circles[i][1] < 0:
-                        del red_circles[i]
-                        break
+                cv2.imshow('Hand Tracking', image)
 
-            cv2.imshow('Hand Tracking', image)
+                key = cv2.waitKey(10)
+                if key & 0xFF == ord('q'):
+                    break
+                elif key & 0xFF == ord('r') and game_over:
+                    score = 0
+                    game_over = False
+                    start_time = time.time()
+                    num_enemies = 1
+                    lives = 3  # Сбросить количество жизней
+                    red_circles.clear()
+                    continue  # Перезапустить основной цикл игры
 
-            key = cv2.waitKey(10)
-            if key & 0xFF == ord('q'):
-                break
-            elif key & 0xFF == ord('r') and game_over:
-                score = 0
-                game_over = False
-                start_time = time.time()
-                num_enemies = 1
-                lives = 3  # Сбросить количество жизней
-                red_circles.clear()
-                continue  # Перезапустить основной цикл игры
+                if score >= level * 10:
+                    level += 1
+                    num_enemies += 1  # Увеличиваем количество врагов на одного
+                    start_time = time.time()
 
-            if score >= level * 10:
-                level += 1
-                num_enemies += 1  # Увеличиваем количество врагов на одного
-                start_time = time.time()
+        video.release()
+        cv2.destroyAllWindows()
+        subprocess.run(["D:/PYTHON_/PROJECT_PYTHON_/otherPY/projDraw3D/venv/Scripts/python.exe", "menu.py"])
+        sys.exit()  # Завершить программу
+    except:
+        video.release()
+        cv2.destroyAllWindows()
+        print("С кружками что то не так")
+        subprocess.run(["D:/PYTHON_/PROJECT_PYTHON_/otherPY/projDraw3D/venv/Scripts/python.exe", "menu.py"])
+        sys.exit()  # Завершить программу
 
-    video.release()
-    cv2.destroyAllWindows()
-    sys.exit()  # Завершить программу
--- menu.py
+++ menu.py
@@ -54,95 +54,98 @@ red_rect = (400, 350, 220, 100)
 blue_rect = (400, 30, 220, 100)
 green_rect = (30, 30, 220, 100)  # Начальные координаты и размеры
 
-while fl:
-    if not game_active:
-        cap = cv2.VideoCapture(0)  # 111111111111111111111111111111111111111111
-        a = cap.set(cv2.CAP_PROP_FPS, frame_rate)
-        print(a)
+try:
+    while fl:
+        if not game_active:
+            cap = cv2.VideoCapture(0)  # 111111111111111111111111111111111111111111
+            a = cap.set(cv2.CAP_PROP_FPS, frame_rate)
+            print(a)
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            cv2.namedWindow("Menu", cv2.WINDOW_NORMAL)
-            cv2.resizeWindow("Menu", 800, 600)
-            frame = cv2.flip(frame, 1)  # 1 означает горизонтальное отражение
+            while cap.isOpened():
+                ret, frame = cap.read()
+                cv2.namedWindow("Menu", cv2.WINDOW_NORMAL)
+                cv2.resizeWindow("Menu", 800, 600)
+                frame = cv2.flip(frame, 1)  # 1 означает горизонтальное отражение
 
-            if not ret:
-                break
+                if not ret:
+                    break
 
-            with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-                frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                results = hands.process(frame_rgb)
+                with mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
+                    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    results = hands.process(frame_rgb)
 
-                if results.multi_hand_landmarks:
-                    for landmarks in results.multi_hand_landmarks:
-                        mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+                    if results.multi_hand_landmarks:
+                        for landmarks in results.multi_hand_landmarks:
+                            mp_drawing.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
 
-                        finger8_x = int(landmarks.landmark[8].x * frame.shape[1])
-                        finger8_y = int(landmarks.landmark[8].y * frame.shape[0])
-                        finger4_x = int(landmarks.landmark[4].x * frame.shape[1])
-                        finger4_y = int(landmarks.landmark[4].y * frame.shape[0])
+                            finger8_x = int(landmarks.landmark[8].x * frame.shape[1])
+                            finger8_y = int(landmarks.landmark[8].y * frame.shape[0])
+                            finger4_x = int(landmarks.landmark[4].x * frame.shape[1])
+                            finger4_y = int(landmarks.landmark[4].y * frame.shape[0])
 
-                        distance = calculate_distance((finger8_x, finger8_y), (finger4_x, finger4_y))
+                            distance = calculate_distance((finger8_x, finger8_y), (finger4_x, finger4_y))
 
-                        if distance < 30:
-                            pointer_finger_closed = True
-                        else:
-                            pointer_finger_closed = False
+                            if distance < 30:
+                                pointer_finger_closed = True
+                            else:
+                                pointer_finger_closed = False
 
-                        if distance < 30:
-                            thumb_finger_closed = True
-                        else:
-                            thumb_finger_closed = False
+                            if distance < 30:
+                                thumb_finger_closed = True
+                            else:
+                                thumb_finger_closed = False
 
-                        # Отслеживание нажатий на прямоугольники и отображение меток
-                        if pointer_finger_closed and thumb_finger_closed:
-                            # Змейка
-                            if rectangle_x < finger8_x < rectangle_x + rectangle_width and rectangle_y < finger8_y < rectangle_y + rectangle_height:
-                                start_game()
-                                fl = False
-                                cv2.destroyWindow("Menu")
-                                break
+                            # Отслеживание нажатий на прямоугольники и отображение меток
+                            if pointer_finger_closed and thumb_finger_closed:
+                                # Змейка
+                                if rectangle_x < finger8_x < rectangle_x + rectangle_width and rectangle_y < finger8_y < rectangle_y + rectangle_height:
+                                    start_game()
+                                    fl = False
+                                    cv2.destroyWindow("Menu")
+                                    break
 
-                            # Пин-Понг
-                            if blue_rect[0] < finger8_x < blue_rect[0] + blue_rect[2] and blue_rect[1] < finger8_y < blue_rect[1] + blue_rect[3]:
-                                cv2.putText(frame, "press 2", (blue_rect[0], blue_rect[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 255), 2)
-                                start_game_2()
-                                fl = False
-                                cv2.destroyWindow("Menu")
-                                break
+                                # Пин-Понг
+                                if blue_rect[0] < finger8_x < blue_rect[0] + blue_rect[2] and blue_rect[1] < finger8_y < blue_rect[1] + blue_rect[3]:
+                                    cv2.putText(frame, "press 2", (blue_rect[0], blue_rect[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 255), 2)
+                                    start_game_2()
+                                    fl = False
+                                    cv2.destroyWindow("Menu")
+                                    break
 
-                            if pink_rect[0] < finger8_x < pink_rect[0] + pink_rect[2] and pink_rect[1] < finger8_y < pink_rect[1] + pink_rect[3]:
-                                cv2.putText(frame, "press 3", (pink_rect[0], pink_rect[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 255), 2)
-                                start_game_3()
-                                fl = False
-                                cv2.destroyWindow("Menu")
-                                break
+                                if pink_rect[0] < finger8_x < pink_rect[0] + pink_rect[2] and pink_rect[1] < finger8_y < pink_rect[1] + pink_rect[3]:
+                                    cv2.putText(frame, "press 3", (pink_rect[0], pink_rect[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 255), 2)
+                                    start_game_3()
+                                    fl = False
+                                    cv2.destroyWindow("Menu")
+                                    break
 
-                            if red_rect[0] < finger8_x < red_rect[0] + red_rect[2] and red_rect[1] < finger8_y < red_rect[1] + red_rect[3]:
-                                if thumb_finger_closed:
-                                    red_rect_clicked = True
-                                if red_rect_clicked:
-                                    cv2.putText(frame, "press 4", (red_rect[0], red_rect[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 255), 2)
+                                if red_rect[0] < finger8_x < red_rect[0] + red_rect[2] and red_rect[1] < finger8_y < red_rect[1] + red_rect[3]:
+                                    if thumb_finger_closed:
+                                        red_rect_clicked = True
+                                    if red_rect_clicked:
+                                        cv2.putText(frame, "press 4", (red_rect[0], red_rect[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 200, 255), 2)
 
 
-            overlay = frame.copy()
-            cv2.putText(frame, "В РАЗРАБОТКЕ", (pink_rect[0], pink_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            cv2.rectangle(overlay, pink_rect[:2], (pink_rect[0] + pink_rect[2], pink_rect[1] + pink_rect[3]), (255, 105, 180), -1)
+                overlay = frame.copy()
+                cv2.putText(frame, "РЕАКЦИЯ", (pink_rect[0], pink_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.rectangle(overlay, pink_rect[:2], (pink_rect[0] + pink_rect[2], pink_rect[1] + pink_rect[3]), (255, 105, 180), -1)
 
-            cv2.putText(frame, "ВИКТОРИНА", (red_rect[0], red_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            cv2.rectangle(overlay, red_rect[:2], (red_rect[0] + red_rect[2], red_rect[1] + red_rect[3]), (0, 0, 255), -1)
+                cv2.putText(frame, "ВИКТОРИНА", (red_rect[0], red_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.rectangle(overlay, red_rect[:2], (red_rect[0] + red_rect[2], red_rect[1] + red_rect[3]), (0, 0, 255), -1)
 
-            cv2.putText(frame, "ПИН-ПОНГ", (blue_rect[0], blue_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            cv2.rectangle(overlay, blue_rect[:2], (blue_rect[0] + blue_rect[2], blue_rect[1] + blue_rect[3]), (255, 0, 0), -1)
+                cv2.putText(frame, "ПИН-ПОНГ", (blue_rect[0], blue_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.rectangle(overlay, blue_rect[:2], (blue_rect[0] + blue_rect[2], blue_rect[1] + blue_rect[3]), (255, 0, 0), -1)
 
-            cv2.putText(frame, "ЗМЕЙКА", (green_rect[0], green_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            cv2.rectangle(overlay, green_rect[:2], (green_rect[0] + green_rect[2], green_rect[1] + green_rect[3]), (0, 255, 0), -1)
+                cv2.putText(frame, "ЗМЕЙКА", (green_rect[0], green_rect[1] + 60), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                cv2.rectangle(overlay, green_rect[:2], (green_rect[0] + green_rect[2], green_rect[1] + green_rect[3]), (0, 255, 0), -1)
 
-            cv2.addWeighted(overlay, 0.5, frame, 1 - 0.5, 0, frame)
+                cv2.addWeighted(overlay, 0.5, frame, 1 - 0.5, 0, frame)
 
-            cv2.imshow("Menu", frame)
+                cv2.imshow("Menu", frame)
 
-            if cv2.waitKey(1) & 0xFF == 27:
-                break
+                if cv2.waitKey(1) & 0xFF == 27:
+                    break
 
-        cap.release()
+            cap.release()
+except:
+    print("Чет не то...")
